00a9804913443f2712c61cf9c87eb7b6ddcbc914
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-06-24 23:27:46 -0600

    Tipos anónimos en consultas linq

--- Etapa1/App/Reporteador.cs
+++ Etapa1/App/Reporteador.cs
@@ -68,13 +68,18 @@ namespace CoreEscuela.App
 
             foreach (var asigConEval in dicEvalXAsig)
             {
-                var dummy = from eval in asigConEval.Value
-                    group eval by eval.Alumno.Unique_Id
+                var promsalumn = from eval in asigConEval.Value
+                    group eval by new {
+                        eval.Alumno.Unique_Id,
+                        eval.Alumno.Name} 
                     into grupoEvalsAlumno
-                    select new {
-                        AlumnoId = grupoEvalsAlumno.Key,
-                        Promedio = grupoEvalsAlumno.Average( evaluacion => evaluacion.Nota)
+                    select new AlumnoPromedio {
+                        alumnoId = grupoEvalsAlumno.Key.Unique_Id,
+                        alumnoNombre = grupoEvalsAlumno.Key.Name,
+                        promedio = grupoEvalsAlumno.Average( evaluacion => evaluacion.Nota)
                     };
+
+                rta.Add(asigConEval.Key,promsalumn);
             }
 
             return rta;
---
+++ Etapa1/Entidades/AlumnoPromedio.cs
@@ -0,0 +1,9 @@
+namespace CoreEscuela.Entidades
+{
+    public class AlumnoPromedio
+    {
+        public double promedio;
+        public string alumnoId;
+        public string alumnoNombre;
+    }
+}
